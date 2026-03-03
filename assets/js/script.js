349838aee2de2ebd943b83b43207adc1c44e2ce5
Xiphoideuss <slaven.loncar1978@gmail.com>
2026-03-03 10:03:23 +0100

    dodana stranica paketi

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,14 @@ navLink.forEach(n => n.addEventListener('click', () => {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show');
 }));
+
+/*===== SCROLL TOP =====*/
+const scrollTop = document.getElementById('scroll-top');
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 300) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll');
+});
+scrollTop.addEventListener('click', e => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
