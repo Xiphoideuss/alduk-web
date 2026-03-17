a7fa9ae5411ed9c1c7b5762669557285f27866eb
Xiphoideuss <slaven.loncar1978@gmail.com>
2026-03-17 11:55:16 +0100

    scroll reveal dodan

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,10 @@ import './nav-menu.js'
 import './scroll-top-btn.js'
 import './lightbox.js'
 import './scroll-indicator.js'
+import {
+  revealLeft,
+  revealRight,
+  revealTop,
+  revealBottom,
+  revealCustom
+} from './scroll-reveal.js';
---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,47 @@
+const sr = ScrollReveal({
+  distance: '120px',
+  duration: 800,
+  easing: 'ease',
+  reset: false,
+});
+
+// GENERIC
+function reveal(selector, options = {}) {
+  sr.reveal(selector, options);
+}
+
+// PRESETOVI
+export function revealLeft(selector) {
+  reveal(selector, { origin: 'left' });
+}
+
+export function revealRight(selector) {
+  reveal(selector, { origin: 'right' });
+}
+
+export function revealTop(selector) {
+  reveal(selector, { origin: 'top' });
+}
+
+export function revealBottom(selector) {
+  reveal(selector, { origin: 'bottom' });
+}
+
+// CUSTOM
+export function revealCustom(selector, options) {
+  reveal(selector, options);
+}
+
+// SEQUENTIAL ANIMATION
+sr.reveal('.equipment__data, .packages__card, .rental-item, .package-wrapper, .gallery-item', {
+  origin: 'left',
+  distance: '50px',
+  duration: 700,
+  interval: 120
+});
+
+// HTML TRIGGERI
+revealLeft('.reveal-left');
+revealRight('.reveal-right');
+revealTop('.reveal-top');
+revealBottom('.reveal-bottom');
